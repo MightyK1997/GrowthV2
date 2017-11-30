05b9196760f0f78ee23798a021e3999d76e32f9b
MightyK1997 <31252492+MightyK1997@users.noreply.github.com>
2017-11-30 12:35:04 -0700

    add project

---
+++ RapidPrototype5/RapidPrototype5/Intermediate/Build/Win64/UE4Editor/Inc/RapidPrototype5/RapidPrototype5GameMode.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef RAPIDPROTOTYPE5_RapidPrototype5GameMode_generated_h
+#error "RapidPrototype5GameMode.generated.h already included, missing '#pragma once' in RapidPrototype5GameMode.h"
+#endif
+#define RAPIDPROTOTYPE5_RapidPrototype5GameMode_generated_h
+
+#define RapidPrototype5_Source_RapidPrototype5_RapidPrototype5GameMode_h_12_RPC_WRAPPERS
+#define RapidPrototype5_Source_RapidPrototype5_RapidPrototype5GameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define RapidPrototype5_Source_RapidPrototype5_RapidPrototype5GameMode_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesARapidPrototype5GameMode(); \
+	friend RAPIDPROTOTYPE5_API class UClass* Z_Construct_UClass_ARapidPrototype5GameMode(); \
+public: \
+	DECLARE_CLASS(ARapidPrototype5GameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/RapidPrototype5"), RAPIDPROTOTYPE5_API) \
+	DECLARE_SERIALIZER(ARapidPrototype5GameMode) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define RapidPrototype5_Source_RapidPrototype5_RapidPrototype5GameMode_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesARapidPrototype5GameMode(); \
+	friend RAPIDPROTOTYPE5_API class UClass* Z_Construct_UClass_ARapidPrototype5GameMode(); \
+public: \
+	DECLARE_CLASS(ARapidPrototype5GameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/RapidPrototype5"), RAPIDPROTOTYPE5_API) \
+	DECLARE_SERIALIZER(ARapidPrototype5GameMode) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define RapidPrototype5_Source_RapidPrototype5_RapidPrototype5GameMode_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	RAPIDPROTOTYPE5_API ARapidPrototype5GameMode(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ARapidPrototype5GameMode) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(RAPIDPROTOTYPE5_API, ARapidPrototype5GameMode); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ARapidPrototype5GameMode); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	RAPIDPROTOTYPE5_API ARapidPrototype5GameMode(ARapidPrototype5GameMode&&); \
+	RAPIDPROTOTYPE5_API ARapidPrototype5GameMode(const ARapidPrototype5GameMode&); \
+public:
+
+
+#define RapidPrototype5_Source_RapidPrototype5_RapidPrototype5GameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	RAPIDPROTOTYPE5_API ARapidPrototype5GameMode(ARapidPrototype5GameMode&&); \
+	RAPIDPROTOTYPE5_API ARapidPrototype5GameMode(const ARapidPrototype5GameMode&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(RAPIDPROTOTYPE5_API, ARapidPrototype5GameMode); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ARapidPrototype5GameMode); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ARapidPrototype5GameMode)
+
+
+#define RapidPrototype5_Source_RapidPrototype5_RapidPrototype5GameMode_h_12_PRIVATE_PROPERTY_OFFSET
+#define RapidPrototype5_Source_RapidPrototype5_RapidPrototype5GameMode_h_9_PROLOG
+#define RapidPrototype5_Source_RapidPrototype5_RapidPrototype5GameMode_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	RapidPrototype5_Source_RapidPrototype5_RapidPrototype5GameMode_h_12_PRIVATE_PROPERTY_OFFSET \
+	RapidPrototype5_Source_RapidPrototype5_RapidPrototype5GameMode_h_12_RPC_WRAPPERS \
+	RapidPrototype5_Source_RapidPrototype5_RapidPrototype5GameMode_h_12_INCLASS \
+	RapidPrototype5_Source_RapidPrototype5_RapidPrototype5GameMode_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define RapidPrototype5_Source_RapidPrototype5_RapidPrototype5GameMode_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	RapidPrototype5_Source_RapidPrototype5_RapidPrototype5GameMode_h_12_PRIVATE_PROPERTY_OFFSET \
+	RapidPrototype5_Source_RapidPrototype5_RapidPrototype5GameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	RapidPrototype5_Source_RapidPrototype5_RapidPrototype5GameMode_h_12_INCLASS_NO_PURE_DECLS \
+	RapidPrototype5_Source_RapidPrototype5_RapidPrototype5GameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID RapidPrototype5_Source_RapidPrototype5_RapidPrototype5GameMode_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
