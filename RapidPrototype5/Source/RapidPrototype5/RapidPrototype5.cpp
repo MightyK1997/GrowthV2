fb405cb2ce88c5277675a495c43a58e8bf84325d
MightyK1997 <31252492+MightyK1997@users.noreply.github.com>
2017-11-30 12:30:16 -0700

    changes

--- RapidPrototype5/Source/RapidPrototype5/RapidPrototype5.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
-
-#include "RapidPrototype5.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RapidPrototype5, "RapidPrototype5" );
- 
