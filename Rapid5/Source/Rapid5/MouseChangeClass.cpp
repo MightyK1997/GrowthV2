bf61d69c288ad0598c092475bb56c84d47ec1453
MightyK1997 <31252492+MightyK1997@users.noreply.github.com>
2017-11-29 18:35:56 -0700

    MOre small changes

---
+++ Rapid5/Source/Rapid5/MouseChangeClass.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MouseChangeClass.h"
+
+MouseChangeClass::MouseChangeClass()
+{
+}
+
+MouseChangeClass::~MouseChangeClass()
+{
+}
---
+++ Rapid5/Source/Rapid5/MouseChangeClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class RAPID5_API MouseChangeClass
+{
+public:
+	MouseChangeClass();
+	~MouseChangeClass();
+};
