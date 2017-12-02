fac019e38d2bfbba9d6885c88de3849772da220d
YuxianDeng <yuxian312@gmail.com>
2017-12-02 16:57:25 -0700

    Fix swing, add mangonel

---
+++ RapidPrototype5/RapidPrototype5/Intermediate/Build/Win64/UE4Editor/Inc/RapidPrototype5/Mangonel.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef RAPIDPROTOTYPE5_Mangonel_generated_h
+#error "Mangonel.generated.h already included, missing '#pragma once' in Mangonel.h"
+#endif
+#define RAPIDPROTOTYPE5_Mangonel_generated_h
+
+#define RapidPrototype5_Source_RapidPrototype5_Mangonel_h_13_RPC_WRAPPERS
+#define RapidPrototype5_Source_RapidPrototype5_Mangonel_h_13_RPC_WRAPPERS_NO_PURE_DECLS
+#define RapidPrototype5_Source_RapidPrototype5_Mangonel_h_13_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAMangonel(); \
+	friend RAPIDPROTOTYPE5_API class UClass* Z_Construct_UClass_AMangonel(); \
+public: \
+	DECLARE_CLASS(AMangonel, AActor, COMPILED_IN_FLAGS(0), 0, TEXT("/Script/RapidPrototype5"), NO_API) \
+	DECLARE_SERIALIZER(AMangonel) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define RapidPrototype5_Source_RapidPrototype5_Mangonel_h_13_INCLASS \
+private: \
+	static void StaticRegisterNativesAMangonel(); \
+	friend RAPIDPROTOTYPE5_API class UClass* Z_Construct_UClass_AMangonel(); \
+public: \
+	DECLARE_CLASS(AMangonel, AActor, COMPILED_IN_FLAGS(0), 0, TEXT("/Script/RapidPrototype5"), NO_API) \
+	DECLARE_SERIALIZER(AMangonel) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define RapidPrototype5_Source_RapidPrototype5_Mangonel_h_13_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AMangonel(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AMangonel) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AMangonel); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMangonel); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AMangonel(AMangonel&&); \
+	NO_API AMangonel(const AMangonel&); \
+public:
+
+
+#define RapidPrototype5_Source_RapidPrototype5_Mangonel_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AMangonel(AMangonel&&); \
+	NO_API AMangonel(const AMangonel&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AMangonel); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMangonel); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AMangonel)
+
+
+#define RapidPrototype5_Source_RapidPrototype5_Mangonel_h_13_PRIVATE_PROPERTY_OFFSET
+#define RapidPrototype5_Source_RapidPrototype5_Mangonel_h_10_PROLOG
+#define RapidPrototype5_Source_RapidPrototype5_Mangonel_h_13_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	RapidPrototype5_Source_RapidPrototype5_Mangonel_h_13_PRIVATE_PROPERTY_OFFSET \
+	RapidPrototype5_Source_RapidPrototype5_Mangonel_h_13_RPC_WRAPPERS \
+	RapidPrototype5_Source_RapidPrototype5_Mangonel_h_13_INCLASS \
+	RapidPrototype5_Source_RapidPrototype5_Mangonel_h_13_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define RapidPrototype5_Source_RapidPrototype5_Mangonel_h_13_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	RapidPrototype5_Source_RapidPrototype5_Mangonel_h_13_PRIVATE_PROPERTY_OFFSET \
+	RapidPrototype5_Source_RapidPrototype5_Mangonel_h_13_RPC_WRAPPERS_NO_PURE_DECLS \
+	RapidPrototype5_Source_RapidPrototype5_Mangonel_h_13_INCLASS_NO_PURE_DECLS \
+	RapidPrototype5_Source_RapidPrototype5_Mangonel_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID RapidPrototype5_Source_RapidPrototype5_Mangonel_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
